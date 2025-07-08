15f8f7c8763e61bc6ef391dd3e1527d853200eff
Kirill <chervotkin.kirill@yandex.ru>
2025-07-08 21:07:39 +0300

    feat: add ItemsListCardBuilder

---
+++ src/builders/itemsListCardBuilder.ts
@@ -0,0 +1,56 @@
+import { Button } from "../interfaces";
+import { Card, ItemsListCard } from "../types/cards";
+import { ItemsListItem } from "../types/cards";
+
+interface ItemsListStepSetHeader {
+    setHeader(text: string): ItemsListStepAddFirstItem;
+}
+
+interface ItemsListStepAddFirstItem {
+    addItem(item: ItemsListItem): ItemsListStepAddMoreItemsOrSetFooter;
+}
+
+interface ItemsListStepAddMoreItemsOrSetFooter {
+    addItem(item: ItemsListItem): ItemsListStepAddMoreItemsOrSetFooter;
+    setFooter(text: string, button: Button): ItemsListStepBuild;
+}
+
+interface ItemsListStepBuild {
+    build(): ItemsListCard;
+}
+
+class ItemsListCardBuilder implements ItemsListStepSetHeader, ItemsListStepAddFirstItem, ItemsListStepAddMoreItemsOrSetFooter, ItemsListStepBuild {
+    private card: Partial<ItemsListCard> = { type: 'ItemsList' };
+    private items: ItemsListItem[] = [];
+
+    private constructor() { }
+
+    static create(): ItemsListStepSetHeader {
+        return new ItemsListCardBuilder();
+    }
+
+    setHeader(text: string): ItemsListStepAddFirstItem {
+        this.card.header = { text };
+        return this;
+    }
+
+    addItem(item: ItemsListItem): ItemsListStepAddMoreItemsOrSetFooter {
+        this.items.push(item);
+        return this;
+    }
+
+    setFooter(text: string, button: Button): ItemsListStepBuild {
+        this.card.footer = { text, button };
+        return this;
+    }
+
+    build(): ItemsListCard {
+        if (!this.card.header || !this.card.footer) {
+            throw new Error("Header and footer must be set");
+        }
+        if (this.items.length < 1 || this.items.length > 5) {
+            throw new Error("Items count must be between 1 and 5");
+        }
+        return this.card as ItemsListCard;
+    }
+}
